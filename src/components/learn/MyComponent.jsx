80af9e4863fc2c59ec3d877c51374bd7686d9bc3
Trong Truong <truongtrong400@gmail.com>
2025-05-13 17:14:11 +0700

    #31 - Code js in HTML with JSX with curly braces

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,17 @@ import './style.css'
 
 // Component = HTML + CSS + JS
 const MyComponent = () =>{
+    // const value = "My First Component 1"; // String
+    // const value = 25; // Number
+    // const value = true; // Boolean
+    // const value = undefined; // Undefined
+    // const value = null; // Null
+    const value = [1, 2, 3]; // Array
+    // const value = {name: "John", age: 25}; // Object
   return (
     <>
-        <div> My First Component Updated</div>
+        <div> {JSON.stringify(value)} Updated</div>
+        <div>{console.log("TT")}</div>
         <div className='child'
             style={{borderRadius: '10px'}}
         >Child</div>
