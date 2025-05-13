a353c163ddc11c0173720a249e40d32419c0adc6
Trong Truong <truongtrong400@gmail.com>
2025-05-13 16:05:55 +0700

    #27 - Refactor App component to use arrow function and encapsulate myFunction

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
+  const myFunction = () => {
+     console.log("Hello World");
+  }
+
+  myFunction();
+  
   return (
     <>
       <div>
