a4e47cd2140749240f17d357bdc33df1c6fe5eee
Trong Truong <truongtrong400@gmail.com>
2025-05-14 11:03:44 +0700

    #36 - DOM Events

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -4,11 +4,26 @@ const TodoNew = (props) =>{
     console.log("Check props",props);
     const {addNewTodo} = props;
     // addNewTodo("Nguyen Van A 1");
+
+    const handleClick = () =>{
+        alert("Click me");
+    }
+
+    const handleOnchange = (name) =>{
+        console.log("Change", name);
+    }
+
     return (
         <>
         <div className='todo-new'>
-          <input type="text" placeholder='Enter your task'/>
-          <button>Add</button>
+          <input type="text" placeholder='Enter your task'
+            onChange={(event) => handleOnchange(event.target.value)} />
+          <button 
+            style={{cursor:"pointer"}}
+            onClick={handleClick}
+          >
+            Add
+          </button>
         </div>
         </>
     )
