329097184f814bc9b8b0f0e865d332f62fd5a5df
Trong Truong <truongtrong400@gmail.com>
2025-05-15 10:45:51 +0700

    #45 - Implement routing with React Router: set up routes for login, register, users, and products pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path:"/login",
+    element: <div>Login Page</div>
+  },
+  {
+    path:"/register",
+    element: <div>Register Page</div>
+  },
+  {
+    path:"/users",
+    element: <div>Users Page</div>
+  },
+  {
+    path:"/products",
+    element: <div>Products Page</div>
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
