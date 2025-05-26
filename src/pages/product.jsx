07b1e556f9f5fd375997aec673de76a839a05545
Trong Truong <truongtrong400@gmail.com>
2025-05-26 10:05:18 +0700

    #46 - Add routing and page components for login, register, users, and products; remove unused CSS files

---
+++ src/pages/product.jsx
@@ -0,0 +1,11 @@
+
+
+const ProductPage = () =>{
+    return (
+        <>
+            <div>Product Page</div>
+        </>
+    )
+}
+
+export default ProductPage;
---
+++ src/pages/register.jsx
@@ -0,0 +1,11 @@
+
+
+const RegisterPage = () =>{
+    return (
+        <>
+            <div>Register Page</div>
+        </>
+    )
+}
+
+export default RegisterPage;
