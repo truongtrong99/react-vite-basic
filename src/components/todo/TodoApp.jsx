24c5182b29c240608dc3676a4ee04bb8b819ba69
Trong Truong <truongtrong400@gmail.com>
2025-05-26 11:05:36 +0700

    #51 - Index Route

---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,73 @@
+import './todo.css'
+import TodoData from './TodoData'
+import TodoNew from './TodoNew'
+import reactLogo from '../../assets/react.svg'
+import { useState } from 'react'
+
+const TodoApp = () =>{
+  const [todoList, setTodoList] = useState([
+    // {id:1, name: "Learning React"},
+    // {id:2, name: "Watching Data"},
+  ]);
+
+  const addNewTodo = (name) =>{
+    const newTodo = {
+      id: randomIntFromInterval(1, 1000000),
+      name: name,
+    }
+    setTodoList([...todoList, newTodo]);
+  }
+
+  const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const deleteTodo = (id) =>{
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  }
+  return (
+    <>
+    <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew 
+          addNewTodo={addNewTodo}
+        />
+        
+        {
+          todoList.length > 0 ?
+          (
+               <TodoData
+                todoList={todoList}
+                deleteTodo={deleteTodo}
+              />
+          )
+          :
+          (
+            <div className="todo-image">
+              <img src={reactLogo} className='logo'/>
+            </div>
+          )
+        }
+
+        {/* {
+          todoList.length > 0 && (
+             <TodoData
+                todoList={todoList}
+              />
+          )
+        }
+        {
+          todoList.length === 0 && (
+            <div className="todo-image">
+              <img src={reactLogo} className='logo'/>
+            </div>
+          )
+        } */}
+      </div>
+
+    </>
+  )
+}
+
+export default TodoApp;
