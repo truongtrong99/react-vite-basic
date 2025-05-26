0a62b016d151f60188d13f44bb8baae307725793
Trong Truong <truongtrong400@gmail.com>
2025-05-26 10:48:55 +0700

    #49 - Client Route with Link

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <>
             <ul>
-                <li><a className="active" href="/">Home</a></li>
-                <li><a href="/users">Users</a></li>
-                <li><a href="/products">Product</a></li>
+                <li><Link className="active" to="/">Home</Link></li>
+                <li><Link to="/users">Users</Link></li>
+                <li><Link to="/products">Product</Link></li>
             </ul>
         </>
     )
