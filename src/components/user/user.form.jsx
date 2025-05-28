2e9a88959051a5d41b67e7b2b016c366dd2f0a07
Trong Truong <truongtrong400@gmail.com>
2025-05-28 18:05:12 +0700

    #73 - Add user management functionality with API integration and environment configuration

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,7 +1,7 @@
-import { Button } from "antd";
+import { Button, notification } from "antd";
 import Input from "antd/es/input/Input";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState('');
@@ -9,22 +9,15 @@ const UserForm = () => {
     const [password, setPassword] = useState('');
     const [phone, setPhone] = useState('');
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = 'http://localhost:8080/api/v1/user';
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if(res.data){
+            notification.success({ message:"Create User", description: 'User created successfully!' })
+        }else{
+            notification.error({ message:"Error Create User", description: JSON.stringify(res.message) })
         }
-        axios.post(URL_BACKEND, data)
-        console.log("Check State >>>", {
-            fullName,
-            email,
-            password,
-            phone
-        });
     }
+    
     return (
         <div className="user-form" style={{ margin: '20px 0' }}>
             <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
