5b923ddf2046630332d59842979104164e331010
Trong Truong <truongtrong400@gmail.com>
2025-05-29 15:20:58 +0700

    #74 - Design Modal Create User

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,4 @@
-import { Button, notification } from "antd";
+import { Button, Modal, notification } from "antd";
 import Input from "antd/es/input/Input";
 import { useState } from "react";
 import { createUserAPI } from "../../services/api.service";
@@ -8,39 +8,53 @@ const UserForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [phone, setPhone] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleClickBtn = async () => {
+    const handleSubmitBtn = async () => {
         const res = await createUserAPI(fullName, email, password, phone)
         if(res.data){
-            notification.success({ message:"Create User", description: 'User created successfully!' })
+            notification.success({ message:"Create User", description: 'User created successfully!' });
+            setIsModalOpen(false);
         }else{
             notification.error({ message:"Error Create User", description: JSON.stringify(res.message) })
         }
     }
     
     return (
-        <div className="user-form" style={{ margin: '20px 0' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                <div>
-                    <span>Full Name</span>
-                    <Input value={fullName} onChange={(event) => setFullName(event.target.value)} />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input value={email} onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <div>
-                    <span>Phone number</span>
-                    <Input value={phone} onChange={(event) => setPhone(event.target.value)} />
-                </div>
-                <div>
-                    <Button type="primary" onClick={handleClickBtn}>Create User</Button>
-                </div>
+        <div className="user-form" style={{ margin: '10px 0' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                <h3>Table Users</h3>
+                <Button type="primary" onClick={()=> setIsModalOpen(true)}>Create User</Button>
             </div>
+
+            <Modal title="Create User" 
+                open={isModalOpen} 
+                onOk={handleSubmitBtn} 
+                onCancel={() => setIsModalOpen(false)} 
+                maskClosable={false} 
+                okText="CREATE"
+                cancelText="Cancel">
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+                        <div>
+                            <span>Full Name</span>
+                            <Input value={fullName} onChange={(event) => setFullName(event.target.value)} />
+                        </div>
+                        <div>
+                            <span>Email</span>
+                            <Input value={email} onChange={(event) => setEmail(event.target.value)} />
+                        </div>
+                        <div>
+                            <span>Password</span>
+                            <Input.Password value={password} onChange={(event) => setPassword(event.target.value)} />
+                        </div>
+                        <div>
+                            <span>Phone number</span>
+                            <Input value={phone} onChange={(event) => setPhone(event.target.value)} />
+                        </div>
+                       
+                    </div>
+            </Modal>
+
         </div>
     )
 }
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -6,7 +6,6 @@ const UserTable = () => {
   const [dataUsers, setDataUsers] = useState([]);
 
   useEffect(() => {
-    console.log("UserTable useEffect");
     loadUser();
   }, []);
 
@@ -24,34 +23,8 @@ const columns = [
     dataIndex: 'email',
   },
 ];
-// const data = [
-//   {
-//     key: '1',
-//     name: 'John Brown',
-//     age: 32,
-//     address: 'New York No. 1 Lake Park',
-//     tags: ['nice', 'developer'],
-//   },
-//   {
-//     key: '2',
-//     name: 'Jim Green',
-//     age: 42,
-//     address: 'London No. 1 Lake Park',
-//     tags: ['loser'],
-//   },
-//   {
-//     key: '3',
-//     name: 'Joe Black',
-//     age: 32,
-//     address: 'Sydney No. 1 Lake Park',
-//     tags: ['cool', 'teacher'],
-//   },
-// ];
-
 const loadUser = async () => {
-  console.log("load user start");
   const res = await fetchAllUserAPI();
-  console.log("load user end", res);
   setDataUsers(res.data);
 }
 
