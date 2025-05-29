e25cbc69deff267a98d9f4ead25e643ffcf3c6c4
Trong Truong <truongtrong400@gmail.com>
2025-05-29 17:27:02 +0700

    #78 - Implement user update functionality with modal and API integration

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,15 +1,34 @@
 import { Link, NavLink } from 'react-router-dom';
 import './header.css';
-
+import { Menu } from 'antd';
+import { BookOutlined, HomeOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+const items = [
+  {
+    label: <Link to={"/"}>Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to={"/users"}>Users</Link>,
+    key: 'users',
+    icon: <UsergroupAddOutlined />,
+  },
+  {
+    label: <Link to={"/books"}>Books</Link>,
+    key: 'books',
+    icon: <BookOutlined />,
+  }
+];
 const Header = () => {
+    const [current, setCurrent] = useState('');
+    const onClick = e => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
     return (
-        <>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/users">Users</NavLink></li>
-                <li><NavLink to="/books">Book</NavLink></li>
-            </ul>
-        </>
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
     )
 }
 
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,11 +1,22 @@
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import { Table } from 'antd';
+import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
 
 const UserTable = (props) => {
-const { dataUsers } = props;
+const { dataUsers, loadUser } = props;
+const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+const [dataUpdate, setDataUpdate] = useState(null);
+
 const columns = [
   {
     title: 'Id',
     dataIndex: '_id',
+    render: (_, record) =>{
+      return (
+        <a href='#'>{record._id}</a>
+      )
+    },
   },
   {
     title: 'Full Name',
@@ -15,11 +26,34 @@ const columns = [
     title: 'Email',
     dataIndex: 'email',
   },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <EditOutlined 
+          onClick={() => {
+            setDataUpdate(record);
+            setIsModalUpdateOpen(true);
+          }}
+         style={{ cursor:'pointer', color:'orange'}}/>
+        <DeleteOutlined style={{ cursor:'pointer', color:'red'}}/>
+      </div>
+    ),
+  },
 ];
 
-
     return (
+       <>
          <Table columns={columns} dataSource={dataUsers} rowKey="_id"/>
+         <UpdateUserModal 
+            isModalUpdateOpen={isModalUpdateOpen} 
+            setIsModalUpdateOpen={setIsModalUpdateOpen}
+            dataUpdate={dataUpdate}
+            setDataUpdate={setDataUpdate} 
+            loadUser={loadUser}
+         />
+       </>
     )
 }
 
