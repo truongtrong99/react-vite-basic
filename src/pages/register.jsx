5c1548f6e3d75b5ce173dd387631b80c827e9122
Trong Truong <truongtrong400@gmail.com>
2025-05-31 15:24:26 +0700

    #90 - Implement user registration functionality with API integration and notifications

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,11 +1,26 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 
 const RegisterPage = () =>{
     const [form] = Form.useForm();
+    const navigate = useNavigate();
 
-    const onFinish = (values) => {
-        console.log('Received values:', values);
+    const onFinish = async (values) => {
+        const res = await registerUserAPI(values.fullName, values.email, values.password, values.phone);
+        if (res && res.data) {
+            notification.success({
+                message: 'Registration Successful',
+                description: 'You have successfully registered!',
+            });
+            navigate('/login'); // Redirect to login page after successful registration
+        } else {
+            notification.error({
+                message: 'Registration Failed',
+                description: JSON.stringify(res.message),
+            });
+        }
     }
     return (
         <>
@@ -19,28 +34,32 @@ const RegisterPage = () =>{
                     <Form.Item
                         label="Full Name"
                         name="fullName"
-                        // rules={[{ required: true, message: 'Please input your full name!' }]}
+                        rules={[{ required: true, message: 'Please input your full name!' }]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="Email"
                         name="email"
-                        // rules={[{ required: true, message: 'Please input your email!' }]}
+                        rules={[{ required: true, message: 'Please input your email!' }]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="Password"
                         name="password"
-                        // rules={[{ required: true, message: 'Please input your password!' }]}
+                        rules={[{ required: true, message: 'Please input your password!' }]}
                     >
                         <Input.Password />
                     </Form.Item>
                     <Form.Item
                         label="Phone Number"
                         name="phone"
-                        // rules={[{ required: true, message: 'Please input your phone number!' }]}
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }]}
                     >
                         <Input />
                     </Form.Item>
