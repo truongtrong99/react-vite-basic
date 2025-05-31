1a6b25945fc0ff232dc94a36d84e02f5856003d2
Trong Truong <truongtrong400@gmail.com>
2025-05-31 14:59:51 +0700

    #89 - Implement registration form with input fields for full name, email, password, and phone number

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,56 @@
+import { Button, Form, Input } from "antd";
 
 
 const RegisterPage = () =>{
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log('Received values:', values);
+    }
     return (
         <>
-            <div>Register Page</div>
+            <Form
+                form={form}
+                layout="vertical"
+                onFinish={onFinish}
+                // onFinishFailed={onFinishFailed}
+            >
+                <div style={{ margin: '50px' }}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        // rules={[{ required: true, message: 'Please input your full name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        // rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        // rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        // rules={[{ required: true, message: 'Please input your phone number!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    
+                    <div>
+                        <Button type="primary" onClick={() => form.submit()}>Register</Button>
+                    </div>
+                </div>
+            </Form>
+            
         </>
     )
 }
