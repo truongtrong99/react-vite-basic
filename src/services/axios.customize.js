0996eca2f9df2b6fccc2ae33461692c6cdc5eb76
Trong Truong <truongtrong400@gmail.com>
2025-06-03 16:49:09 +0700

    #113 - Add NProgress for loading indicators and integrate with Axios requests

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -10,12 +16,14 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+    NProgress.start();
     if (typeof window !== "undefined" && window && window.localStorage && window.localStorage.getItem('access_token')) {
         config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
     }
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done();
     // Do something with request error
     return Promise.reject(error);
 });
@@ -23,6 +31,7 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done();
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if (response.data && response.data.data) {
@@ -30,6 +39,7 @@ instance.interceptors.response.use(function (response) {
     }
     return response;
   }, function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if( error.response && error.response.data) {
